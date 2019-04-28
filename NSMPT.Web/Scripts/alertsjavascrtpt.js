12f8c1127cf5138912bf53622c5c67a6942f2ab5
Near <274733956@qq.com>
2019-04-29 01:35:41 +0800

    增加提示信息

--- NSMPT.Web/Scripts/alertsjavascrtpt.js
+++ NSMPT.Web/Scripts/alertsjavascrtpt.js
@@ -1,41 +1,55 @@
 ﻿
 
+
+
+
 function alert_success(message)
 {
-    $("#div_alert").html("");
-  
-    var html = "<div class=\"alert alert-success alert-dismissible\">  <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">×</button> <h4><i class=\"icon fa fa-check\"></i> Alert!</h4> " + message + " </div > ";
-    $("#div_alert").html(html);
-    $("#div_alert").hide();
-    $("#div_alert").show().delay(2000).hide(300);
+
+    $(function () {
+
+        $.bootstrapGrowl(message, {
+            ele: 'body', // which element to append to
+            type: 'success', // (null, 'info', 'danger', 'success')
+            offset: { from: 'top', amount: 60 }, // 'top', or 'bottom'
+            align: 'center', // ('left', 'right', or 'center')
+            width: 250, // (integer, or 'auto')
+            delay: 3000, // Time while the message will be displayed. It's not equivalent to the *demo* timeOut!
+            allow_dismiss: true, // If true then will display a cross to close the popup.
+            stackup_spacing: 10 // spacing between consecutively stacked growls.
+        });
+    });
    
 
 }
 
 
 function alert_danger(message) {
-    $("#div_alert").html("");
-    var html = "<div class=\"alert alert-danger alert-dismissible\">  <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">×</button> <h4><i class=\"icon fa fa-check\"></i> Alert!</h4> " + message + " </div > ";
-    $("#div_alert").html(html);
-    $("#div_alert").hide();
-    $("#div_alert").show().delay(3000).hide(300);
+    $.bootstrapGrowl(message, {
+        ele: 'body', // which element to append to
+        type: 'danger', // (null, 'info', 'danger', 'success')
+        offset: { from: 'top', amount: 70 }, // 'top', or 'bottom'
+        align: 'center', // ('left', 'right', or 'center')
+        width: 250, // (integer, or 'auto')
+        delay: 3000, // Time while the message will be displayed. It's not equivalent to the *demo* timeOut!
+        allow_dismiss: true, // If true then will display a cross to close the popup.
+        stackup_spacing: 10 // spacing between consecutively stacked growls.
+    });
+
 
 }
 
 function alert_info(message) {
-    $("#div_alert").html("");
-    var html = "<div class=\"alert alert-info alert-dismissible\">  <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">×</button> <h4><i class=\"icon fa fa-check\"></i> Alert!</h4> " + message + " </div > ";
-    $("#div_alert").html(html);
-    $("#div_alert").hide();
-    $("#div_alert").show().delay(2000).hide(300);
+    $.bootstrapGrowl(message, {
+        ele: 'body', // which element to append to
+        type: 'info', // (null, 'info', 'danger', 'success')
+        offset: { from: 'top', amount: 70 }, // 'top', or 'bottom'
+        align: 'center', // ('left', 'right', or 'center')
+        width: 250, // (integer, or 'auto')
+        delay: 3000, // Time while the message will be displayed. It's not equivalent to the *demo* timeOut!
+        allow_dismiss: true, // If true then will display a cross to close the popup.
+        stackup_spacing: 10 // spacing between consecutively stacked growls.
+    });
 
 }
 
-function alert_warning(message) {
-    $("#div_alert").html("");
-    var html = "<div class=\"alert alert-warning alert-dismissible\">  <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">×</button> <h4><i class=\"icon fa fa-check\"></i> Alert!</h4> " + message + " </div > ";
-    $("#div_alert").html(html);
-    $("#div_alert").hide();
-    $("#div_alert").show().delay(2000).hide(300);
-
-}
